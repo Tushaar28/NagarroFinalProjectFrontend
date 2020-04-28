aabb79438f3cfc0472c866a68f4ffe82dacd21e7
Tushaar28 <tushaartiwari1997@gmail.com>
2020-04-28 21:27:03 +0530

    integrated create ticket with backend

--- ticketApp/src/app/cnfticket/cnfticket.component.ts
+++ ticketApp/src/app/cnfticket/cnfticket.component.ts
@@ -1,6 +1,5 @@
 import { CreateticketComponent } from './../createticket/createticket.component';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-cnfticket',
   templateUrl: './cnfticket.component.html',
@@ -9,12 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CnfticketComponent implements OnInit {
 
   constructor(ticket: CreateticketComponent) { 
-    console.log(ticket.arr);
   }
 
   ngOnInit(): void {
   }
-
-
-
 }
--- ticketApp/src/app/createticket/createticket.component.ts
+++ ticketApp/src/app/createticket/createticket.component.ts
@@ -2,39 +2,40 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'app-createticket',
   templateUrl: './createticket.component.html',
   styleUrls: ['./createticket.component.css']
 })
-export class CreateticketComponent implements OnInit {
+export class CreateticketComponent {
   arr: any;
-
+  error: Boolean = false;
+  //data: Response;
   constructor(private router: Router,
     private http: HttpClient) {
     //const battuta_url_countries = "http://battuta.medunes.net/api/country/all/?key=d3efa1d399e23647962f8d096894d1ca"
    }
-
-  ngOnInit(): void {
-    
-  }
 
   onClickReset(){
     this.router.navigate['/home'];
   }
 
   submit(values){
-    this.arr = values;
-    const headers = {'Authorization': 'Bearer ' + localStorage.getItem('token'), 'id': localStorage.getItem('id')};
-    // headers.set('Authorization', 'Bearer ' + localStorage.getItem('token'));
-    // headers.set('Id', localStorage.getItem('id'));
-    // headers.set('Accept', 'application/json');
+    const headers = {'Authorization': 'Bearer ' + localStorage.getItem('token'), 'id': localStorage.getItem('id'), 'Accept': 'application/json'};
     const url = 'http://localhost:8040/addticket'
-    let response = this.http.post(url, values, { headers });
-    response.subscribe((data: Response)=>console.log(data))
-    setTimeout(()=> this.router.navigate(['/cnfticket']), 3000);
+    let response = this.http.post(url, values, { 'headers': headers });
+    response.subscribe(
+      data => {
+        console.log(data);
+        this.arr = data;
+      },
+      error => this.error = true
+      )
+    if(!this.error)
+      setTimeout(() => {
+        this.router.navigate(['/cnfticket'])
+      }, 1000);
   }
 
 }
